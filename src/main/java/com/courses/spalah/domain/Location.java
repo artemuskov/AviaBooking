71efed3f5b8972bd98787d71b0213e015b061805
Artem Uskov <uskov.artem@gmail.com>
2016-11-17 19:41:08 +0200

    171116-1940-notebook

---
+++ src/main/java/com/courses/spalah/domain/Location.java
@@ -0,0 +1,12 @@
+package com.courses.spalah.domain;
+
+/**
+ * Created by artem on 17.11.2016.
+ */
+public class Location {
+
+    private String country;
+    private String city;
+    private double Latitude;
+    private double Longitute;
+}
---
+++ src/main/java/com/courses/spalah/domain/Route.java
@@ -0,0 +1,14 @@
+package com.courses.spalah.domain;
+
+import java.util.Date;
+
+/**
+ * Created by artem on 17.11.2016.
+ */
+public class Route {
+
+    private Location outPoint;
+    private Location inPoint;
+    private Date outTime;
+    private Date inTime;
+}
