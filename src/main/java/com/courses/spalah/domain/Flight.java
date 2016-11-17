22dc55b85f33965ada7c082e03534548292fae42
Artem Uskov <uskov.artem@gmail.com>
2016-11-17 19:19:05 +0200

    171116-1930-notebook

---
+++ src/main/java/com/courses/spalah/domain/Flight.java
@@ -0,0 +1,13 @@
+package com.courses.spalah.domain;
+
+import java.util.ArrayList;
+
+/**
+ * Created by artem on 17.11.2016.
+ */
+public class Flight {
+
+    private String flightNumber;
+    private Plane currentPlane;
+    private ArrayList<Route> listRoutes;
+}
